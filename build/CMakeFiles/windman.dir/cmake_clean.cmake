9b6244f1e87e6ee83b761230ea9e5918ae0c4c73
system <ali_expres@list.ru>
2020-03-17 13:47:50 +0500

    base

---
+++ build/CMakeFiles/windman.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/windman.dir/main.cpp.o"
+  "windman.pdb"
+  "windman"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/windman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
